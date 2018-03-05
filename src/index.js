f04eef828bc4abd25280ee8011ce016c053414a0
Chris McNeese <chris@85supply.com>
2018-03-04 23:57:51 -0600

    add Board constructor

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ this.setState({value: 'X'})}>
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            squares: Array(9).fill(null),
+        };
+    }
   renderSquare(i) {
     return <Square value={i} />;
   }
